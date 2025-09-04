4a2c7820ac3ebb42a120ea999b10f5cc0caaa8b5
Matthew Rae <mattyjrae123@hotmail.com>
2025-09-04 12:09:13 +1200

    Move Model and View into Controller file

--- src/main/resources/static/scripts/controller/controller.js
+++ src/main/resources/static/scripts/controller/controller.js
@@ -1,6 +1,11 @@
+import Model from "../model/Model.js";
+import View from "../view/View.js";
+
 const Controller = (() => {
   const start = () => {
-    console.log("Hello, from Controller.start()")
+    console.log("Hello, from Controller.start()");
+    Model.start();
+    View.start();
   }
 
   return {
--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -3,7 +3,4 @@ import Model from "./model/Model.js";
 import View from "./view/View.js";
 import Controller from "./controller/controller.js";
 
-console.log("Hello, WORLD!!");
-Model.start();
-View.start();
 Controller.start();
