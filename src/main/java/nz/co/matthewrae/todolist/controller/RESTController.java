09324535fb9bc70a5ac38044f94363ce324090f8
Matthew Rae <mattyjrae123@hotmail.com>
2025-09-04 16:58:12 +1200

    Add restcontroller boilerplate

---
+++ src/main/java/nz/co/matthewrae/todolist/controller/RESTController.java
@@ -0,0 +1,22 @@
+package nz.co.matthewrae.todolist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class RESTController {
+  JdbcTemplate jdbcTemplate;
+  
+  public RESTController(@Autowired JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @GetMapping("/lists")
+  public String getAllLists() {
+      return new String();
+  }
+  
+}
